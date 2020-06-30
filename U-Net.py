342ddff1e25996bcaa69f3f40e28171166bacd1a
Herbie007 <19720152203409@stu.xmu.edu.cn>
2020-06-30 13:12:55 +0800

    unet

--- U-Net.py
+++ U-Net.py
@@ -26,56 +26,56 @@ class UNet(nn.Module):
             nn.Conv2d(basic_channels, basic_channels, 3),
             nn.ReLU()
         )
-        self.pool1 = nn.MaxPool2d(2)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
         self.down2 = nn.Sequential(
             nn.Conv2d(basic_channels, basic_channels * 2, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 2, basic_channels * 2, 3),
             nn.ReLU()
         )
-        self.pool2 = nn.MaxPool2d(2)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
         self.down3 = nn.Sequential(
             nn.Conv2d(basic_channels * 2, basic_channels * 4, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 4, basic_channels * 4, 3),
             nn.ReLU()
         )
-        self.pool3 = nn.MaxPool2d(2)
+        self.pool3 = nn.MaxPool2d(2, stride=2)
         self.down4 = nn.Sequential(
             nn.Conv2d(basic_channels * 4, basic_channels * 8, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 8, basic_channels * 8, 3),
             nn.ReLU()
         )
-        self.pool4 = nn.MaxPool2d(2)
+        self.pool4 = nn.MaxPool2d(2, stride=2)
         self.plain = nn.Sequential(
             nn.Conv2d(basic_channels * 8, basic_channels * 16, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 16, basic_channels * 16, 3),
             nn.ReLU()
         )
-        self.upconv1 = nn.ConvTranspose2d(basic_channels * 16, basic_channels * 8, 2)
+        self.upconv1 = nn.ConvTranspose2d(basic_channels * 16, basic_channels * 8, 2, 2)
         self.up1 = nn.Sequential(
             nn.Conv2d(basic_channels * 16, basic_channels * 8, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 8, basic_channels * 8, 3),
             nn.ReLU()
         )
-        self.upconv2 = nn.ConvTranspose2d(basic_channels * 8, basic_channels * 4, 2)
+        self.upconv2 = nn.ConvTranspose2d(basic_channels * 8, basic_channels * 4, 2, 2)
         self.up2 = nn.Sequential(
             nn.Conv2d(basic_channels * 8, basic_channels * 4, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 4, basic_channels * 4, 3),
             nn.ReLU()
         )
-        self.upconv3 = nn.ConvTranspose2d(basic_channels * 4, basic_channels * 2, 2)
+        self.upconv3 = nn.ConvTranspose2d(basic_channels * 4, basic_channels * 2, 2, 2)
         self.up3 = nn.Sequential(
             nn.Conv2d(basic_channels * 4, basic_channels * 2, 3),
             nn.ReLU(),
             nn.Conv2d(basic_channels * 2, basic_channels * 2, 3),
             nn.ReLU()
         )
-        self.upconv4 = nn.ConvTranspose2d(basic_channels * 2, basic_channels, 2)
+        self.upconv4 = nn.ConvTranspose2d(basic_channels * 2, basic_channels, 2, 2)
         self.up4 = nn.Sequential(
             nn.Conv2d(basic_channels * 2, basic_channels, 3),
             nn.ReLU(),
@@ -83,10 +83,6 @@ class UNet(nn.Module):
             nn.ReLU(),
             nn.Conv2d(basic_channels, out_channels, 1)
         )
-        self.center_crop1 = torchvision.transforms.CenterCrop(size6)
-        self.center_crop2 = torchvision.transforms.CenterCrop(size7)
-        self.center_crop3 = torchvision.transforms.CenterCrop(size8)
-        self.center_crop4 = torchvision.transforms.CenterCrop(size9)
 
     def forward(self, img):
         temp1 = self.down1(img)
@@ -94,14 +90,34 @@ class UNet(nn.Module):
         temp3 = self.down3(self.pool2(temp2))
         temp4 = self.down4(self.pool3(temp3))
         img = self.upconv1(self.plain(self.pool4(temp4)))
-        img = torch.cat((self.center_crop1(temp4), img), dim=0)
+        deltaY = temp4.shape[2] - img.shape[2]
+        deltaX = temp4.shape[3] - img.shape[3]
+        img = nn.functional.pad(img, [deltaX//2, deltaX - deltaX//2,
+                                      deltaY//2, deltaY - deltaY//2])
+        img = torch.cat((temp4, img), dim=1)
         img = self.upconv2(self.up1(img))
-        img = torch.cat((self.center_crop2(temp3), img), dim=0)
+        deltaY = temp3.shape[2] - img.shape[2]
+        deltaX = temp3.shape[3] - img.shape[3]
+        img = nn.functional.pad(img, [deltaX // 2, deltaX - deltaX // 2,
+                                      deltaY // 2, deltaY - deltaY // 2])
+        img = torch.cat((temp3, img), dim=1)
         img = self.upconv3(self.up2(img))
-        img = torch.cat((self.center_crop3(temp2), img), dim=0)
+        deltaY = temp2.shape[2] - img.shape[2]
+        deltaX = temp2.shape[3] - img.shape[3]
+        img = nn.functional.pad(img, [deltaX // 2, deltaX - deltaX // 2,
+                                      deltaY // 2, deltaY - deltaY // 2])
+        img = torch.cat((temp2, img), dim=1)
         img = self.upconv4(self.up3(img))
-        img = torch.cat((self.center_crop4(temp1), img), dim=0)
+        deltaY = temp1.shape[2] - img.shape[2]
+        deltaX = temp1.shape[3] - img.shape[3]
+        img = nn.functional.pad(img, [deltaX // 2, deltaX - deltaX // 2,
+                                      deltaY // 2, deltaY - deltaY // 2])
+        img = torch.cat((temp1, img), dim=1)
         img = self.up4(img)
         return img
 
+u = UNet()
+a = torch.rand((1, 1, 572, 572))
+a = u(a)
+print(a.shape)
 
